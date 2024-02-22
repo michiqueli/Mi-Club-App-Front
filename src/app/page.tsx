3a670a92b624b7ffb923281ffcddc2e3012f4bbd
michqiueli <michiqueli@gmail.com>
2024-02-22 20:17:25 -0300

    readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import NovedadesSlider from '../components/NoverdadesSlider'
 
-export default function Home() {
+export default function Home () {
   return (
     <main className='mt-32 w-[95%] sm:w-[60%] h-full'>
       <NovedadesSlider/>
